52b5e6d923a20b57839b9a4b532cec2c8f6688f2
60482 <lp1033@wscloud.org>
2026-01-21 13:32:32 -0500

    Add files via upload

---
+++ Dust.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dust : MonoBehaviour
+{
+    [SerializeField]
+    private Animator _animator;
+
+    private AnimatorStateInfo _stateInfo;
+
+    public event Action OnFinish;
+
+    void Update ()
+    {
+        _stateInfo = _animator.GetCurrentAnimatorStateInfo(0);
+
+        if (_stateInfo.normalizedTime >= 1f)
+        {
+            if(OnFinish != null)
+            {
+                OnFinish();
+            }
+        }
+    }
+}
